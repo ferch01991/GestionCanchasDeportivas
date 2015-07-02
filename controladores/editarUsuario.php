e9d57ed4bf073d05388cbfc892ec0c8f30f286e7
Erik Santiago <esantiago@localhost.localdomain>
2015-07-02 09:35:05 -0500

    Correciones formulario edicion usuario y creaciones grupo

--- controladores/editarUsuario.php
+++ controladores/editarUsuario.php
@@ -9,17 +9,14 @@ extract($_POST);
 @$target_path = "../imagenes/usuarios/";
 @$target_path = $target_path . basename( $_FILES['imagen']['name']); 
 if(move_uploaded_file($_FILES['imagen']['tmp_name'], $target_path)) { 
-	#header("location:../vistas/muro.php");
 	$imagenUsuario = $target_path;
 }else{
 	echo "Ha ocurrido un error, trate de nuevo!";
 }
-
-
-
 $sql = "UPDATE usuarios 
-		SET nombres = '$nombres', apellidos = '$apellidos', email = '$email', password = '$password', sexo = '$sexo', telefono = '$telefono', direccion = '$direccion', acerca = '$acerca', imagen = '$imagenUsuario'
+		SET nombres = '$nombres', apellidos = '$apellidos', password = '$password', telefono = '$telefono', direccion = '$direccion', acerca = '$acerca', imagen = '$imagenUsuario'
 		WHERE id = '$id'";
 $conexion->ejecutar($sql);
+chmod($imagenUsuario, 0755); 
 header("location:../vistas/muro.php?id=$id");
 ?>
--- static/site_config.php
+++ static/site_config.php
@@ -2,5 +2,5 @@
 $db_host="127.0.0.1";
 $db_user="root";
 $db_password="";
-$db_name="cancha";
+$db_name="sistemacanchas";
 ?>
