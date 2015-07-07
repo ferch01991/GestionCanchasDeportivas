102f47f2eddad1627ecd9a48813b34db2d5655f0
ferch01991 <faherrera2@utpl.edu.ec>
2015-07-07 12:44:33 -0500

    Mensaje de alerta al reservar una cancha

--- controladores/crearPartido.php
+++ controladores/crearPartido.php
@@ -2,19 +2,24 @@
 include ("../static/site_config.php");
 include ("../static/clase_mysql.php");
 $conexion = new clase_mysql;
+echo "string";
 $conexion->conectar($db_name,$db_host, $db_user,$db_password);
 extract($_POST);
 $canOcupadas = $conexion->controlCancha($cancha, $fecha, $hora);
 if ($canOcupadas == 0) {
-$sql = "INSERT INTO partidos VALUES ('','$idGrupo','$cancha','$fecha','$hora','','')";
-$conexion->ejecutar($sql);	
-header("location:../vistas/grupo.php?idGrupo=".$idGrupo."&idUsuario=".$idUsuario."");
+	$sql = "INSERT INTO partidos VALUES ('','$idGrupo','$cancha','$fecha','$hora','','')";
+	$conexion->ejecutar($sql);	
+	header("location:../vistas/grupo.php?idGrupo=".$idGrupo."&idUsuario=".$idUsuario."");
 }else{
-	echo "<script language='javascript'>"; 
-	echo "alert('Error!! Torna a identificar-te. Les dades no són correctes.')"; 
-	echo "</script>";  
+	echo "<script>
+
+	function alerta(){
+      alertify.alert('Blog Reaccion Estudio probando Alertify', function () {
+      });
+	";
+}
 	header("location:../vistas/grupo.php?idGrupo=".$idGrupo."&idUsuario=".$idUsuario."");
 }
-}
+
 ?>
 
