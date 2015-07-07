7e327a106f941db0c088f8f61cb4e92213448551
ferch01991 <faherrera2@utpl.edu.ec>
2015-07-07 15:16:53 -0500

    Mensaje de alerta al reservar una cancha 2

--- controladores/crearPartido.php
+++ controladores/crearPartido.php
@@ -2,24 +2,18 @@
 include ("../static/site_config.php");
 include ("../static/clase_mysql.php");
 $conexion = new clase_mysql;
-echo "string";
 $conexion->conectar($db_name,$db_host, $db_user,$db_password);
 extract($_POST);
 $canOcupadas = $conexion->controlCancha($cancha, $fecha, $hora);
 if ($canOcupadas == 0) {
 	$sql = "INSERT INTO partidos VALUES ('','$idGrupo','$cancha','$fecha','$hora','','')";
-	$conexion->ejecutar($sql);	
+	$conexion->ejecutar($sql);
 	header("location:../vistas/grupo.php?idGrupo=".$idGrupo."&idUsuario=".$idUsuario."");
 }else{
-	echo "<script>
-
-	function alerta(){
-      alertify.alert('Blog Reaccion Estudio probando Alertify', function () {
-      });
-	";
+	echo "<script type='text/javascript'>";
+	echo "alert('La cancha selecionada no esta disponible a esa hora');";
+	echo "location.href = '../vistas/grupo.php?idGrupo=".$idGrupo."&idUsuario=".$idUsuario."';";
+	echo "</script>";
 }
-	header("location:../vistas/grupo.php?idGrupo=".$idGrupo."&idUsuario=".$idUsuario."");
-}
-
 ?>
 
